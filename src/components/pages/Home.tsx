acfad6421854858b6ef63e0d3832b39a3544b4f7
Shrinivasan M <msrini1100@gmail.com>
2025-05-31 18:10:21 +0530

    Initial commit

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Home = () => {
+  return (
+    <a href="/markdown-page" className="p-4 underline">
+      Markdown is also supported...
+    </a>
+  );
+};
+
+export default Home;
